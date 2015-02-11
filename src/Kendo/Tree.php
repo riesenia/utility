0caaee288903577ffbb45497c23c1d7b4a2a6e0d
segy <segy@riesenia.com>
2015-02-11 14:58:52 +0100

    added tree

---
+++ src/Kendo/Tree.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Riesenia\Utility\Kendo;
+
+use Riesenia\Kendo\Kendo;
+
+/**
+ * Tree helper
+ *
+ * @author Tomas Saghy <segy@riesenia.com>
+ */
+class Tree extends KendoHelper
+{
+    /**
+     * Row template
+     *
+     * @var string
+     */
+    protected $_rowTemplate;
+
+    /**
+     * Table columns
+     *
+     * @var array
+     */
+    protected $_columns;
+
+    /**
+     * Actions
+     *
+     * @var array
+     */
+    protected $_actions;
+
+    /**
+     * Construct the tree
+     *
+     * @param string id
+     */
+    public function __construct($id)
+    {
+        parent::__construct($id);
+
+        $this->model = Kendo::createModel()
+            ->setId('id');
+
+        $this->dataSource = Kendo::createHierarchicalDataSource()
+            ->setSchema([
+                'model' => $this->model,
+                'data' => 'results',
+                'parse' => Kendo::js('function (data) {
+                    // prevent duplicates
+                    var ids = $("#' . $id . '").data("ids");
+                    if (typeof ids === "undefined")
+                        ids = [];
+
+                    ret = [];
+
+                    for (var i in data) {
+                        if ($.inArray(data[i].id, ids) > -1)
+                            continue;
+
+                        ids.push(data[i].id);
+                        ret.push(data[i]);
+                    }
+                    $("#' . $id . '").data("ids", ids);
+
+                    return ret;
+                }')
+            ]);
+
+        $this->_widget = Kendo::createTreeView('#' . $id)
+            ->setDataSource($this->dataSource)
+            ->setDataTextField('name');
+    }
+
+    /**
+     * Add transport (passed to datasource)
+     *
+     * @param string type
+     * @param array options
+     * @return Riesenia\Utility\Kendo\Tree
+     */
+    public function addTransport($type, $options = [])
+    {
+        $this->dataSource->addTransport($type, $options);
+
+        return $this;
+    }
+
+    /**
+     * Add checkboxes
+     *
+     * @param mixed checkboxes options
+     * @return Riesenia\Utility\Kendo\Tree
+     */
+    public function addCheckboxes($options = null)
+    {
+        // default template
+        if (is_null($options)) {
+            $options = ['template' => '<input type="checkbox" value="#: item.id #" />'];
+        }
+
+        $this->_widget->setCheckboxes($options);
+
+        return $this;
+    }
+
+    /**
+     * Return HTML
+     *
+     * @return string
+     */
+    public function html()
+    {
+        return '<div id="' . $this->_id . '"></div>';
+    }
+
+    /**
+     * Return JavaScript
+     *
+     * @return string
+     */
+    public function script()
+    {
+        $script = $this->_widget->__toString();
+
+        return $script;
+    }
+}
