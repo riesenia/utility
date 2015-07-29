60445d5190755e7be62a0229cbf461e77bd0fcb8
segy <segy@riesenia.com>
2015-07-29 17:21:54 +0200

    multi select

---
+++ src/Kendo/MultiSelect.php
@@ -0,0 +1,56 @@
+<?php
+namespace Riesenia\Utility\Kendo;
+
+use Riesenia\Kendo\Kendo;
+
+/**
+ * MultiSelect helper
+ *
+ * @author Tomas Saghy <segy@riesenia.com>
+ */
+class MultiSelect extends KendoHelper
+{
+    /**
+     * Construct the select
+     *
+     * @param string id
+     */
+    public function __construct($id)
+    {
+        parent::__construct($id);
+
+        $this->dataSource = Kendo::createDataSource()
+            ->setSchema(['data' => 'results', 'total' => 'count'])
+            ->setServerFiltering(true)
+            ->setServerPaging(true);
+
+        $this->_widget = Kendo::createMultiSelect('#' . $id)
+            ->setDataSource($this->dataSource)
+            ->setDataValueField('id')
+            ->setDataTextField('name');
+
+        $this->setAttribute('name', $id);
+    }
+
+    /**
+     * Return HTML
+     *
+     * @return string
+     */
+    public function html()
+    {
+        return $this->_input($this->_id);
+    }
+
+    /**
+     * Return JavaScript
+     *
+     * @return string
+     */
+    public function script()
+    {
+        $script = $this->_widget->__toString();
+
+        return $script;
+    }
+}
