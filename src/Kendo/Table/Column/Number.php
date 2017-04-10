d8999d93325f9e0611a02fa8fd7fc02f0e60f3ea
Masaker000 <martin.kassay@gmail.com>
2017-04-10 15:13:17 +0200

    Update Number.php

--- src/Kendo/Table/Column/Number.php
+++ src/Kendo/Table/Column/Number.php
@@ -21,4 +21,23 @@ class Number extends Base
      * @var string
      */
     protected $_class = 'tableColumn tableNumber';
+
+    /**
+     * Construct the column
+     *
+     * @param array options
+     * @param string table id
+     */
+    public function __construct(array $options, $tableId)
+    {
+        if (isset($options['arrow']) && $options['arrow']) {
+            $this->_format = '# if (%field% < 0) {#<span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span>#} else if (%field% > 0) {#<span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span>#}#' . $this->_format;
+        }
+
+        if (isset($options['colorize']) && $options['colorize']) {
+            $this->_format = '<span class="#: %field% < 0 ? "negative" : (%field% > 0 ? "positive" : "neutral") #">' . $this->_format . '</span>';
+        }
+
+        parent::__construct($options, $tableId);
+    }
 }
