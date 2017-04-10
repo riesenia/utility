4f95ea4a23ea28989ba5872b8736d5b9bfdd7363
segy <segy@riesenia.com>
2017-04-10 15:58:12 +0200

    arrow & colorize

--- src/Kendo/Table/Column/Number.php
+++ src/Kendo/Table/Column/Number.php
@@ -21,4 +21,31 @@ class Number extends Base
      * @var string
      */
     protected $_class = 'tableColumn tableNumber';
+
+    /**
+     * Construct the column
+     *
+     * @param array options
+     * @param string table id
+     */
+    public function __construct(array $options, $tableId)
+    {
+        if (isset($options['arrow']) && $options['arrow']) {
+            if (!isset($options['compare_field'])) {
+                $options['compare_field'] = '0';
+            }
+
+            $this->_format = '# if (%field% < ' . $options['compare_field'] . ') { #<span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span># } else if (%field% > ' . $options['compare_field'] . ') { #<span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span># } # ' . $this->_format;
+        }
+
+        if (isset($options['colorize']) && $options['colorize']) {
+            if (!isset($options['compare_field'])) {
+                $options['compare_field'] = '0';
+            }
+
+            $this->_format = '<span class="#: %field% < ' . $options['compare_field'] . ' ? "tableColumnNegative" : (%field% > ' . $options['compare_field'] . ' ? "tableColumnPositive" : "tableColumnNeutral") #">' . $this->_format . '</span>';
+        }
+
+        parent::__construct($options, $tableId);
+    }
 }
