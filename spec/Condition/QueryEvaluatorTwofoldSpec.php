699cf9271f086bfe3cd037854280722367dffd4c
John Diegor <johnd@cloudstaff.com>
2017-09-13 23:26:19 +0800

    initial commit

---
+++ spec/Condition/QueryEvaluatorTwofoldSpec.php
@@ -0,0 +1,90 @@
+<?php
+namespace spec\Riesenia\Utility\Condition;
+
+use PhpSpec\ObjectBehavior;
+use Riesenia\Utility\Condition\QueryEvaluatorException;
+
+class QueryEvaluatorTwofoldSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith([
+            'P1' => [
+                'pid' => [
+                    'field' => 'id',
+                    'operators' => ['=', 'NOT', 'IN', 'NOTIN']
+                ],
+                'name' => [
+                    'field' => 'name',
+                    'operators' => ['=', 'NOT', 'CONTAINS']
+                ],
+                'price' => [
+                    'field' => 'unit_price',
+                    'operators' => ['>=', '>', '<', '<='] ]
+                ],
+            'P2' => [
+                'pid' => [
+                    'field' => 'id',
+                    'operators' => ['=', 'NOT', 'IN', 'NOTIN']
+                ],
+                'name' => [
+                    'field' => 'name',
+                    'operators' => ['=', 'NOT', 'CONTAINS']
+                ],
+                'price' => [
+                    'field' => 'unit_price',
+                    'operators' => ['>=', '>', '<', '<=']
+                ]
+            ]
+        ]);
+    }
+
+    public function it_parses_simple_conditions()
+    {
+        $this->parse('P1.pid = 56')->shouldReturn(['P1.id' => '56']);
+        $this->parse('P1.pid NOT 3')->shouldReturn(['P1.id !=' => '3']);
+        $this->parse('P1.pid IN 2, 3')->shouldReturn(['P1.id IN' => ['2', '3']]);
+        $this->parse('P2.name CONTAINS abcd etc')->shouldReturn(['P2.name LIKE' => '%abcd etc%']);
+    }
+
+    public function it_parses_and_condition()
+    {
+        $this->parse('P1.pid IN 2, 3 AND P2.price >= 10')->shouldReturn(['AND' => [
+            ['P1.id IN' => ['2', '3']],
+            ['P2.unit_price >=' => '10']
+        ]]);
+    }
+
+    public function it_parses_or_condition()
+    {
+        $this->parse('P2.pid IN 2, 3 OR P1.price >= 10')->shouldReturn(['OR' => [
+            ['P2.id IN' => ['2', '3']],
+            ['P1.unit_price >=' => '10']
+        ]]);
+    }
+
+    public function it_parses_and_or_condition_with_correct_precedence()
+    {
+        $this->parse('P1.pid IN 2, 3 AND P1.price >= 10 OR P1.name = x')->shouldReturn(['OR' => [
+            ['AND' => [
+                ['P1.id IN' => ['2', '3']],
+                ['P1.unit_price >=' => '10']
+            ]],
+            ['P1.name' => 'x']
+        ]]);
+    }
+
+    public function it_parses_complex_condition_with_parenthesis()
+    {
+        $this->parse('P1.pid IN 2, 3 AND ((P1.price >= 10 OR P1.name = x) OR P1.name CONTAINS y)')->shouldReturn(['AND' => [
+            ['P1.id IN' => ['2', '3']],
+            ['OR' => [
+                ['OR' => [
+                    ['P1.unit_price >=' => '10'],
+                    ['P1.name' => 'x']
+                ]],
+                ['P1.name LIKE' => '%y%']
+            ]]
+        ]]);
+    }
+}
---
+++ src/Condition/QueryEvaluatorTwofold.php
@@ -0,0 +1,69 @@
+<?php
+namespace Riesenia\Utility\Condition;
+
+/**
+ * This class will enable to provide configuration for two independent “prefixes”.
+ *
+ * Constructor accepts configuration array in format:
+ *     'prefix' => [
+ *         'placeholder' => [
+ *             'field' => 'field name',
+ *             'operators' => [available operators]
+ *         ]
+ *     ]
+ *
+ * @author John Diegor <john.diegor@gmail.com>
+ */
+class QueryEvaluatorTwofold extends QueryEvaluator
+{
+    /**
+     * Setup
+     *
+     * @param array configuration options
+     */
+    public function __construct(array $config)
+    {
+        if (!is_array($config)) {
+            throw new QueryEvaluatorException('Invalid config format');
+            exit(0);
+        }
+
+        $this->_config = $config;
+    }
+
+    /**
+     * Parse condition (column operator value)
+     *
+     * @param string condition
+     * @return array
+     */
+    public function parseCondition($condition)
+    {
+        $condition = array_map('trim', explode(' ', $condition, 3));
+
+        if (count($condition) < 3) {
+            throw new QueryEvaluatorException(['placeholder' => $condition[0]], QueryEvaluatorException::INVALID_CONDITION);
+        }
+
+        list($column, $operator, $value) = $condition;
+
+        // check if field has a prefix
+        if (strpos($column, '.') !== false) {
+            list($prefix, $column) = explode('.', $column);
+
+            if (!isset($this->_config[$prefix][$column])) {
+                throw new QueryEvaluatorException(['placeholder' => "{$prefix}.{$column}"], QueryEvaluatorException::UNKNOWN_PLACEHOLDER);
+            }
+
+            if (!in_array($operator, $this->_config[$prefix][$column]['operators'])) {
+                throw new QueryEvaluatorException(['placeholder' => "{$prefix}.{$column}", 'operator' => $operator], QueryEvaluatorException::UNKNOWN_OPERATOR);
+            }
+
+            $parsedCondition = $this->_parseCondition($prefix.".".$this->_config[$prefix][$column]['field'], $operator, $value);
+        } else {
+            throw new QueryEvaluatorException('The column requires a prefix');
+        }
+
+        return $parsedCondition;
+    }
+}
